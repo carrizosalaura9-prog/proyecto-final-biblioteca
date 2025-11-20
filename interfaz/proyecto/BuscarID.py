846f9175f99767274efcc8f93f72e2960f5d69d9
Laura Carrizosa <carrizosalaura9@gmail.com>
2025-11-20 10:05:20 -0700

    #

---
+++ interfaz/proyecto/BuscarID.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+def Encontrar_ID():
+    #ID=[12345,54321]
+
+    # if entrada_ID.get() in ID:
+    root = tk.Tk() 
+    root.title("INSTITUTO TECNOLOGICO DE SONORA")
+    root.geometry("800x600")
+
+    root.mainloop()
+
+
